d150d997af09564624257bf333e3455445d3235d
Shishir Tiwari <shishir456tiwari@gmail.com>
2022-01-17 12:24:58 +0530

    Input Field tsx code added

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,7 +1,23 @@
-const InputField = () => {
+import { ChangeEventHandler, HTMLInputTypeAttribute } from "react";
+import styles from "./InputField.module.scss";
+
+interface Props{
+    id:string;
+    label:string;
+    type:HTMLInputTypeAttribute;
+    placeholder?:string;
+    disabled?:boolean;
+    value:string ;
+    onChange:(event:React.ChangeEvent<HTMLInputElement>)=>void;
+    [x:string]:any;
+}
+
+const InputField = (props:Props) => {
+    const {id,type,label,placeholder,disabled,value,onChange,...rest}=props;
     return (
         <div>
-            This is InputField
+            <label htmlFor={id}>{label}</label>
+            <input id={id} type={type} placeholder={placeholder} disabled={disabled} value={value} onChange={onChange} {...rest}/>
         </div>
     )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,15 @@
-import { Button } from "../../components";
+import { Button, InputField } from "../../components";
+import {useState} from "react";
+
 
 const Home = () => {
+const [name,setName]=useState<string>("");
   return (
     <div>
       <section>
         <h1>Button</h1>
         <Button>Btn</Button>
+        <InputField id="some-button" type="text" label="Name" value={name} onChange={(e)=>setName(e.target.value)} />
       </section>
     </div>
   );
