cc28bb4919f4f40e8bfe07bb1eddd6bb02cdf14b
Anurag Pal <apal895@gmail.com>
2022-01-17 12:46:53 +0530

    formatting|linting

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,25 +1,34 @@
 import { ChangeEventHandler, HTMLInputTypeAttribute } from "react";
 import styles from "./InputField.module.scss";
 
-interface Props{
-    id:string;
-    label:string;
-    type:HTMLInputTypeAttribute;
-    placeholder?:string;
-    disabled?:boolean;
-    value:string ;
-    onChange:(event:React.ChangeEvent<HTMLInputElement>)=>void;
-    [x:string]:any;
+interface Props {
+  id: string;
+  label: string;
+  type: HTMLInputTypeAttribute;
+  placeholder?: string;
+  disabled?: boolean;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  [x: string]: any;
 }
 
-const InputField = (props:Props) => {
-    const {id,type,label,placeholder,disabled,value,onChange,...rest}=props;
-    return (
-        <div>
-            <label htmlFor={id}>{label}</label>
-            <input id={id} type={type} placeholder={placeholder} disabled={disabled} value={value} onChange={onChange} {...rest}/>
-        </div>
-    )
-}
+const InputField = (props: Props) => {
+  const { id, type, label, placeholder, disabled, value, onChange, ...rest } =
+    props;
+  return (
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        disabled={disabled}
+        value={value}
+        onChange={onChange}
+        {...rest}
+      />
+    </div>
+  );
+};
 
-export default InputField
+export default InputField;
