16a3310e0912dc105961e320bf07edab70a4c110
Anurag Pal <apal895@gmail.com>
2022-02-28 08:58:02 +0530

    added mui grid t

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,9 +1,41 @@
-const Test = () => {
-    return (
-        <div>
-            Test
-        </div>
-    )
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarExport,
+  GridToolbarDensitySelector,
+} from "@mui/x-data-grid";
+import { useDemoData } from "@mui/x-data-grid-generator";
+
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarColumnsButton touchRippleRef={null} />
+      <GridToolbarFilterButton />
+      <GridToolbarDensitySelector touchRippleRef={null} />
+      <GridToolbarExport touchRippleRef={null} />
+    </GridToolbarContainer>
+  );
 }
 
-export default Test
+const Test = () => {
+  const { data } = useDemoData({
+    dataSet: "Commodity",
+    rowLength: 10,
+    maxColumns: 6,
+  });
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        {...data}
+        components={{
+          Toolbar: CustomToolbar,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Test;
