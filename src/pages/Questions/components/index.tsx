17fae0244046d7771090f56067798974018dcf0c
Anurag Pal <apal895@gmail.com>
2022-03-05 15:17:32 +0530

    added paragraph type

---
+++ src/pages/Questions/components/index.tsx
@@ -0,0 +1,166 @@
+import styles from "../Questions.module.scss";
+
+import {
+  TextField,
+  FormControl,
+  MenuItem,
+  Select,
+  InputLabel,
+  Chip,
+  Autocomplete,
+  SelectChangeEvent,
+} from "@mui/material";
+import { styled } from "@mui/system";
+
+interface MUISelectProps {
+  label: string;
+  state: string;
+  options: Array<{
+    name: string;
+    value: string;
+  }>;
+  onChange: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const MUISelect = (props: MUISelectProps) => {
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    props.onChange(event.target.value);
+  };
+  return (
+    <StyledFormControl sx={{ minWidth: 250 }}>
+      <InputLabel id="demo-simple-select-helper-label">
+        {props.label}
+      </InputLabel>
+      <Select
+        labelId="demo-simple-select-helper-label"
+        id="demo-simple-select-helper"
+        value={props.state}
+        label="Age"
+        onChange={handleChange}
+      >
+        {props.options.map((item, index) => (
+          <MenuItem key={index} value={item.value}>
+            {item.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </StyledFormControl>
+  );
+};
+
+interface MUIAutocompleteProps {
+  label: string;
+  state?: string;
+  onChange: any;
+  options: Array<{
+    name: string;
+    value: string;
+  }>;
+}
+
+export const MUIChipsAutocomplete = (props: MUIAutocompleteProps) => {
+  return (
+    <Autocomplete
+      multiple
+      id="tags-outlined"
+      onChange={(_, value) =>
+        props.onChange(
+          value.map((item) => {
+            return item.value;
+          })
+        )
+      }
+      options={props.options}
+      getOptionLabel={(option) => option.name}
+      filterSelectedOptions
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={props.label}
+          placeholder={"Search for " + props.label}
+        />
+      )}
+    />
+  );
+};
+
+export const MUISimpleAutocomplete = (props: MUIAutocompleteProps) => {
+  return (
+    <Autocomplete
+      className={styles.something}
+      disablePortal
+      id="combo-box-demo"
+      options={props.options}
+      onChange={(_, value) => props.onChange(value?.value || "")}
+      getOptionLabel={(option) => option.name || ""}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          placeholder={"Search for" + props.label}
+          label={props.label}
+        />
+      )}
+    />
+  );
+};
+
+const StyledMUITextField = styled(TextField)(() => {
+  return {
+    minWidth: "250px",
+    input: {
+      fontSize: "1rem",
+      padding: "1.2rem 1.3rem",
+    },
+    label: {
+      fontSize: "1rem",
+      maxWidth: "none",
+      padding: "0rem 0.5rem",
+      backgroundColor: " #f3f3f9",
+    },
+    ".MuiInputLabel-root.Mui-focused": {
+      transform: "translate(12px, -9px) scale(0.75)",
+    },
+    ".MuiFormLabel-filled": {
+      transform: "translate(12px, -9px) scale(0.75)",
+    },
+  };
+});
+
+export const StyledMUISelect = styled(MUISelect)(() => {
+  return {
+    minWidth: "250px",
+    input: {
+      fontSize: "1rem",
+      padding: "1.2rem 1.3rem",
+    },
+
+    ".MuiInputLabel-root.Mui-focused": {
+      transform: "translate(12px, -9px) scale(0.75)",
+    },
+    ".MuiFormLabel-filled": {
+      transform: "translate(12px, -9px) scale(0.75)",
+    },
+  };
+});
+
+const StyledFormControl = styled(FormControl)(() => {
+  return {
+    minWidth: "250px",
+    input: {
+      fontSize: "1rem",
+      padding: "1.2rem 1.3rem",
+    },
+    label: {
+      fontSize: "1rem",
+      maxWidth: "none",
+      padding: "0rem 0.5rem",
+      backgroundColor: " #f3f3f9",
+    },
+    ".MuiInputLabel-root.Mui-focused": {
+      transform: "translate(12px, -9px) scale(0.75)",
+    },
+    ".MuiFormLabel-filled": {
+      transform: "translate(12px, -9px) scale(0.75)",
+    },
+  };
+});
