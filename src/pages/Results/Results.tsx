162bfdd0118363bcfd8e6ae187fbe5267e9e4bd9
Shishir Tiwari <shishirtiwari@Shishirs-MacBook-Air.local>
2022-03-12 12:11:39 +0530

    result ui improved

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,7 +1,7 @@
 import styles from "./Results.module.scss";
 import { useParams, useNavigate } from "react-router";
 import { useEffect, useState } from "react";
-import Students from "../Users/Students/Students";
+import { Button, Sidebar, NotificationCard } from "../../components/";
 
 const tests = [
   {
@@ -32,7 +32,7 @@ const Results = () => {
     if (!status) return;
     switch (status.toLowerCase()) {
       case "ongoing": {
-        return "green";
+        return "var(--clr-success)";
       }
 
       default: {
@@ -53,7 +53,9 @@ const Results = () => {
       </div>
       <div className={styles.content}>
         <div className={styles.top}>
-          <h1>{currentTest?.name}</h1>
+          <h2>
+            {currentTest?.name} ({currentTest?.exam?.fullName})
+          </h2>
           <div className={styles.status}>
             {currentTest?.status}{" "}
             <div
@@ -63,14 +65,38 @@ const Results = () => {
           </div>
         </div>
 
-        <h3>Exam : {currentTest?.exam?.fullName}</h3>
-        <h2>Marks Obtained : 180/360</h2>
+        <div className={styles.basicInfo}>
+          <h3 className={styles.marksObtained}>
+            Marks Obtained :{" "}
+            <span className={styles.boldLarge}>{23 * 3 * 4}/360</span>
+          </h3>
+          <h3 className={styles.totalAttempted}>
+            Attempted : <span className={styles.boldLarge}>{23 * 3}</span>{" "}
+          </h3>
+        </div>
         <div className={styles.cards}>
           <SubjectCard />
           <SubjectCard />
           <SubjectCard />
         </div>
+        <Button disabled color="success">
+          View Detailed Analysis
+        </Button>
       </div>
+      <Sidebar title="Recent Activity">
+        {Array(10)
+          .fill(0)
+          .map((_, i) => (
+            <NotificationCard
+              key={i}
+              id="aasdadsd"
+              status={i % 2 === 0 ? "success" : "warning"}
+              title={"New Student Joined-" + i}
+              description="New student join IIT Pulse Anurag Pal - Dropper Batch"
+              createdAt="10 Jan, 2022"
+            />
+          ))}
+      </Sidebar>
     </div>
   );
 };
@@ -80,8 +106,28 @@ interface ISubjectCard {}
 const SubjectCard = (props: ISubjectCard) => {
   return (
     <div className={styles.subjectCard}>
-      <h4>Physics</h4>
-      <h5>Attempted : 23/30</h5>
+      <h3 className={styles.subjectName}>Physics</h3>
+      <p>
+        Marks Obtained : <span className={styles.boldSmall}>{23 * 4}</span>
+      </p>
+      <p>
+        Attempted : <span className={styles.boldSmall}>23/30</span>
+      </p>{" "}
+      <div className={styles.moreInfo}>
+        <p>
+          Correct : <span className={styles.boldSmall}> 23</span>
+        </p>
+        <p>
+          Incorrect : <span className={styles.boldSmall}>0</span>
+        </p>
+        <p>
+          Accuracy : <span className={styles.boldSmall}>100%</span>
+        </p>
+        <p>
+          Time Elapsed : <span className={styles.boldSmall}>1:00:00</span>
+        </p>
+      </div>
+      <div className={styles.circularProgress}></div>
     </div>
   );
 };
