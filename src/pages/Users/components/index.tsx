cbc8d916cf405e46670ac259d1dfb3b896e8cc72
Shishir Tiwari <shishir456tiwari@gmail.com>
2022-06-21 20:22:44 +0530

    user section fields and autcomplete with creatable select added

--- src/pages/Users/components/index.tsx
+++ src/pages/Users/components/index.tsx
@@ -1,5 +1,17 @@
-import { TextField } from "@mui/material";
+import {
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  Button,
+  Autocomplete,
+} from "@mui/material";
+import { createFilterOptions } from "@mui/material/Autocomplete";
+import styles from "./index.module.scss";
 import { styled } from "@mui/system";
+import { useEffect, useState } from "react";
 
 export const StyledMUITextField = styled(TextField)(() => {
   return {
@@ -46,3 +58,163 @@ export interface UserProps {
   id: string;
   handleReset: () => void;
 }
+
+// ----------------------------------------------------------------------------
+
+interface DropdownOptionType {
+  name: string;
+  inputValue?: any;
+  value: any;
+}
+const filter = createFilterOptions<DropdownOptionType>();
+
+interface MUICreatableSelectProps {
+  options: Array<DropdownOptionType>;
+  label: string;
+  id: string;
+  value: any;
+  onChange: any;
+}
+
+export const MUICreatableSelect: React.FC<MUICreatableSelectProps> = ({
+  options,
+  label,
+  id,
+  onChange,
+  value,
+}) => {
+  // const [value, setValue] = useState<DropdownOptionType | null>(null);
+  const [open, toggleOpen] = useState(false);
+  useEffect(() => {
+    console.log({ value });
+  });
+  const handleClose = () => {
+    setDialogValue({
+      name: "",
+      inputValue: "",
+    });
+    toggleOpen(false);
+  };
+
+  const [dialogValue, setDialogValue] = useState({
+    name: "",
+    inputValue: "",
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // setValue({
+    //   name: dialogValue.name,
+    //   value: parseInt(dialogValue.inputValue, 10),
+    // });
+    onChange(event, {
+      name: dialogValue.name,
+      value: parseInt(dialogValue.inputValue, 10),
+    });
+    handleClose();
+  };
+  return (
+    <div className={styles.creatableSelect}>
+      <StyledMUIAutocompleteForCreatableSelect
+        id={id}
+        value={value ? value.name : ""}
+        onChange={(event, newValue: any) => {
+          console.log({ newValue });
+          if (newValue && newValue.inputValue) {
+            toggleOpen(true);
+            setDialogValue({
+              name: newValue.value,
+              inputValue: "",
+            });
+          } else {
+            // setValue(newValue);
+            onChange(event, newValue);
+          }
+        }}
+        filterOptions={(options: any, params) => {
+          const filtered = filter(options, params);
+          if (params.inputValue !== "") {
+            filtered.push({
+              inputValue: params.inputValue,
+              name: `Add "${params.inputValue}"`,
+              value: params.inputValue,
+            });
+          }
+
+          return filtered;
+        }}
+        options={options}
+        getOptionLabel={(option: any) => {
+          // e.g value selected with enter, right from the input
+          if (typeof option === "string") {
+            return option;
+          }
+          if (option.value) {
+            return option.value;
+          }
+          return option.name;
+        }}
+        selectOnFocus
+        clearOnBlur
+        handleHomeEndKeys
+        renderOption={(props, option: any) => <li {...props}>{option.name}</li>}
+        sx={{ width: 300 }}
+        freeSolo
+        renderInput={(params) => <TextField {...params} label={label} />}
+      />
+      <Dialog open={open} onClose={handleClose}>
+        <form onSubmit={handleSubmit}>
+          <DialogTitle>Add a New "{label}"</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Did you miss any "{label}" in our list? Please, add it!
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              value={dialogValue.name}
+              onChange={(event) =>
+                setDialogValue({
+                  ...dialogValue,
+                  name: event.target.value,
+                })
+              }
+              label="Name"
+              type="text"
+              variant="standard"
+            />
+            <TextField
+              margin="dense"
+              id="name"
+              value={dialogValue.inputValue}
+              onChange={(event) =>
+                setDialogValue({
+                  ...dialogValue,
+                  inputValue: event.target.value,
+                })
+              }
+              label="Value"
+              type="text"
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type="submit">Add</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </div>
+  );
+};
+
+export const StyledMUIAutocompleteForCreatableSelect = styled(Autocomplete)(
+  () => {
+    return {
+      "& .MuiAutocomplete-endAdornment": {
+        top: 0,
+      },
+    };
+  }
+);
