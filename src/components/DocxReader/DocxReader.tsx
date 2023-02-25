1c8717cd8a0c6c047f0f10447b260c72545291ba
Anurag Pal <apal895@gmail.com>
2023-02-25 19:27:00 +0530

    working with tables using the DOM api

--- src/components/DocxReader/DocxReader.tsx
+++ src/components/DocxReader/DocxReader.tsx
@@ -2,24 +2,50 @@ import React, { useEffect, useState } from "react";
 // @ts-ignore
 import docx2html from "docx2html";
 
-function DocxReader(): JSX.Element {
-  const [html, setHtml] = useState("");
+function DocxReader() {
+  const [tableData, setTableData] = useState<string[][][]>([]);
 
   const readFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    setHtml("");
-    const file: File = e.target.files![0];
-    const result = await docx2html(file);
-    setHtml(result.toString());
+    const file = e.target.files?.[0];
+    const html = await docx2html(file as File);
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html.toString(), "text/html");
+    const tables = doc.getElementsByTagName("table");
+    const tableData: string[][][] = [];
+    for (let i = 0; i < tables.length; i++) {
+      const rows = tables[i].rows;
+      const tableRows: string[][] = [];
+      for (let j = 0; j < rows.length; j++) {
+        const cells = rows[j].cells;
+        const rowData: string[] = [];
+        for (let k = 0; k < cells.length; k++) {
+          rowData.push(cells[k].innerText);
+        }
+        tableRows.push(rowData);
+      }
+      tableData.push(tableRows);
+    }
+    setTableData(tableData);
   };
 
-  useEffect(() => {
-    console.log("html", html);
-  }, [html]);
+  useEffect(() => {}, []);
 
   return (
     <div>
       <input type="file" onChange={readFile} />
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      {tableData.map((table, index) => (
+        <table key={index}>
+          <tbody>
+            {table.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((cell, cellIndex) => (
+                  <td key={cellIndex}>{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ))}
     </div>
   );
 }
