ced3be330b2814d2f5971399ff03bf314aac3cda
Anurag Pal <apal895@gmail.com>
2023-07-18 10:29:31 +0530

    mui to antd migrated design, login logic with loading done

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,33 +1,16 @@
-import { useState, useContext, useEffect } from "react";
-// import { Button, InputField } from "../../components";
+import { useContext, useEffect } from "react";
 import styles from "./Login.module.scss";
 import { decodeToken } from "react-jwt";
-import axios from "axios";
 import { AuthContext } from "../../utils/auth/AuthContext";
 import { useNavigate } from "react-router";
 import logo from "../../assets/images/logo.svg";
-import { LinearProgress, TextField } from "@mui/material";
 import { API_USERS } from "../../utils/api/config";
 import { AUTH_TOKEN } from "../../utils/constants";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputAdornment from "@mui/material/InputAdornment";
-import { Visibility } from "@mui/icons-material";
-import { VisibilityOff } from "@mui/icons-material";
-import IconButton from "@mui/material/IconButton";
-import { Link } from "react-router-dom";
-import { Button, Checkbox, Form, Input, message } from "antd";
+import { Button, Form, Input, message } from "antd";
 
 const Login = () => {
   const { currentUser, setCurrentUser } = useContext(AuthContext);
   const navigate = useNavigate();
-
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>("");
-  const [showPassword, setShowPassword] = useState<boolean>(false);
 
   useEffect(() => {
     console.log(currentUser);
@@ -36,21 +19,22 @@ const Login = () => {
     }
   }, [currentUser]);
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    setLoading(true);
-    message.loading({ content: "Logging in", key: "loader" });
+  async function handleSubmit({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) {
+    let loading = message.loading({ content: "Logging in", key: "loader" });
     try {
       const response = await API_USERS().post(`/auth/login/`, {
         email,
         password,
       });
 
-      console.log({ decoded: decodeToken(response.data.token), response });
-
       if (response.status === 200) {
         let decoded = decodeToken(response.data.token) as any;
-        console.log({ decoded });
         let newRoles: any = {};
         decoded?.roles?.forEach((role: any) => {
           newRoles[role.id] = {
@@ -66,22 +50,29 @@ const Login = () => {
           roles: newRoles,
         });
         localStorage.setItem(AUTH_TOKEN, response.data.token);
-        setLoading(false);
+        loading();
+        message.success("Logged in with " + decoded.email);
         navigate("/", { replace: true });
       } else {
       }
     } catch (error: any) {
       console.log("True error", error);
       if (error?.response?.data) {
-        setError(error.response.data.message);
+        loading();
         message.error(error.response.data.message);
       } else {
+        loading();
         message.error("Network Error");
-        setError("Network Error");
       }
-      setLoading(false);
     }
-    message.destroy("loader");
+  }
+
+  function handleClickSignup() {
+    navigate("/student-register", { replace: true });
+  }
+
+  function handleClickForgotPassword() {
+    navigate("/reset-password", { replace: true });
   }
 
   return (
@@ -89,47 +80,50 @@ const Login = () => {
       <div className={styles.formContainer}>
         <img src={logo} className={styles.logo} alt="iitpulse" />
         <p>Please enter your email and password</p>
-        <Form
-          name="basic"
-          layout="vertical"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          // onFinish={onFinish}
-          // onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: "Please input your email!" }]}
+        <div>
+          <Form
+            name="login"
+            layout="vertical"
+            onFinish={handleSubmit}
+            autoComplete="off"
+            className={styles.form}
           >
-            <Input />
-          </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input />
+            </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{ offset: 8, span: 16 }}
-          >
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className={styles.submitBtn}
+              >
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+          <div className={styles.extras}>
+            <Button type="default" onClick={handleClickSignup}>
+              Sign Up
             </Button>
-          </Form.Item>
-        </Form>
+            <Button onClick={handleClickForgotPassword} type="link">
+              Forgot Password?
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
