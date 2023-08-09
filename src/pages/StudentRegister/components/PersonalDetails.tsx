a9b2db1f7fbe93baeb15582121f528f61a54edb3
Anurag Pal <apal895@gmail.com>
2023-08-09 12:02:02 +0530

    initial data correction

--- src/pages/StudentRegister/components/PersonalDetails.tsx
+++ src/pages/StudentRegister/components/PersonalDetails.tsx
@@ -27,7 +27,6 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import { API_USERS } from "../../../utils/api/config";
 
-
 const validateMessages = {
   required: "${label} is required!",
   types: {
@@ -57,8 +56,8 @@ const defaultState = {
   city: "",
   state: "",
   parentName: "",
-  parentContact: 0,
-  contact: 0,
+  parentContact: "",
+  contact: "",
   address: "",
 };
 
